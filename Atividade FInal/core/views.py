c2d017289601cfabeb8a578d890b8e4a178a3100
Beatriz Cristina Marques <beatrizmaarqueszo@gmail.com>
2022-12-15 00:24:42 -0300

    :)

---
+++ Atividade FInal/core/views.py
@@ -0,0 +1,10 @@
+# core/views.py
+from rest_framework import viewsets
+
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
